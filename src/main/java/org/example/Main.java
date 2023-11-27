f31402164608ad219048ecbfee3fa33ebbc90f9d
Agustin Vera <agustin.vera@usach.cl>
2023-11-26 21:05:55 -0300

    Se agrega la clase Option en conjunto con el RF1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Laboratorio 3: Paradigmas\n");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // ######################### Chatbot 0 #####################################
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Opciones - Flujo 1 - Chatbot 0
+        ArrayList<String> keywordOp1F1CB0 = new ArrayList<>(Arrays.asList("viajar", "turistear", "conocer"));
+        var op1F1CB0 = new Option_21065666_VeraRojas(1, "1) Viajar", 1, 1, keywordOp1F1CB0);
+
+        ArrayList<String> keywordOp2F1CB0 = new ArrayList<>(Arrays.asList("estudiar", "aprender", "perfeccionarme"));
+        var op2F1CB0 = new Option_21065666_VeraRojas(2, "2) Estudiar", 2, 1, keywordOp2F1CB0);
+
+        System.out.println(op1F1CB0);
+        System.out.println(op2F1CB0);
     }
 }
---
+++ src/main/java/org/example/Option_21065666_VeraRojas.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Option_21065666_VeraRojas {
+
+    private int code;
+    private String message;
+    private int chatbotCodeLink;
+    private int initialFlowCodeLink;
+    private ArrayList<String> keywords;
+
+
+    public Option_21065666_VeraRojas(int code, String message, int chatbotCodeLink, int initialFlowCodeLink, ArrayList<String> keywords) {
+        this.code = code;
+        this.message = message;
+        this.chatbotCodeLink = chatbotCodeLink;
+        this.initialFlowCodeLink = initialFlowCodeLink;
+        this.keywords = keywords;
+    }
+
+    @Override
+    public String toString() {
+        return "Option_21065666_VeraRojas{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", chatbotCodeLink=" + chatbotCodeLink +
+                ", initialFlowCodeLink=" + initialFlowCodeLink +
+                ", keywords=" + keywords +
+                '}';
+    }
+}
