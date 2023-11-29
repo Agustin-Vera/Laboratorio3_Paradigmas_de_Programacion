34e41d31f999171fc075238146f26303c5f1e7c1
Agustin Vera <agustin.vera@usach.cl>
2023-11-28 22:39:34 -0300

    Se realizan cambios en el RF2, se implementa el RF3 y se cambia el Project encoding a UTF-8 debido a que la consola de IntelliJ IDEA estaba imprimiendo Strings de manera rara, esto provoco la creacion por parte del IDE de varios archivos dentro de la carpeta .idea

--- src/main/java/org/example/Flow_21065666_VeraRojas.java
+++ src/main/java/org/example/Flow_21065666_VeraRojas.java
@@ -8,29 +8,44 @@ public class Flow_21065666_VeraRojas {
     private String nameMsg;
     private ArrayList<Option_21065666_VeraRojas> options;
 
+    // ########### RF2 - TDA Flow - Constructor ################
     public Flow_21065666_VeraRojas(int id, String nameMsg, ArrayList<Option_21065666_VeraRojas> options) {
-        if(this.uniqueOptions(options)) {
             this.id = id;
             this.nameMsg = nameMsg;
-            this.options = options;
-        }else {
-            this.id = id;
-            this.nameMsg = nameMsg;
-            this.options = new ArrayList<>();
-        }
+            this.options = getUniqueOptions(options);
     }
 
-    // Verifica si una lista de options no posee ninguna option repetida
-    public boolean uniqueOptions(ArrayList<Option_21065666_VeraRojas> options){
+    // Obtiene las primeras ocurrencias de cada Option en una lista
+    public ArrayList<Option_21065666_VeraRojas> getUniqueOptions(ArrayList<Option_21065666_VeraRojas> options){
         for(int i = 0; i < options.size(); i++){
             for(int j = i+1; j < options.size(); j++){
                 if(options.get(i).getCode() == options.get(j).getCode()){
-                    System.out.println("Existe al menos una opcion repetida a la hora de crear el flujo, pruebe con otras opciones no repetidas.");
-                    return false;
+                    options.remove(j);
                 }
             }
         }
-        return true;
+        return options;
+    }
+
+    // Verifica si una option existe en el flow
+    public boolean optionExist(Option_21065666_VeraRojas newOption){
+        for(Option_21065666_VeraRojas option: this.options){
+            if(option.getCode() == newOption.getCode()){
+                return true;
+            }
+        }
+        return false;
+    }
+    // ########### RF3 - TDA flowAddOption - Modificador #######
+    // ESTO LO EJECUTA EL USUARIO ADMINISTRADOR
+    public void flowAddOption(Option_21065666_VeraRojas option){
+        if(this.optionExist(option)){
+            System.out.println("Ya existe una opcion con ese identificador, pruebe con otra.");
+            //return;
+        }else {
+            this.options.add(option);
+            System.out.println("Opcion " + option.getCode() + " fue agregada con exito.");
+        }
     }
 
     @Override
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Laboratorio 3: Paradigmas\n");
+        System.out.println("Laboratorio 3: Paradigmas\n");
 
         // ######################### Chatbot 0 #####################################
 
@@ -20,8 +20,27 @@ public class Main {
 
         // Flujo 1 - Chatbot 0
         ArrayList<Option_21065666_VeraRojas> optionsFlow1CB0 = new ArrayList<>(Arrays.asList(op1F1CB0, op2F1CB0));
-        var f1CB0 = new Flow_21065666_VeraRojas(1, "Flujo Principal Chatbot 1\nBienvenido\n¿Que te gustaria hacer?", optionsFlow1CB0);
+        var f1CB0 = new Flow_21065666_VeraRojas(1, "Flujo Principal Chatbot 1\nBienvenido\n¿Qué te gustaría hacer?", optionsFlow1CB0);
 
-        System.out.println(f1CB0);
+        // ######################### Chatbot 1 #####################################
+
+        // Opciones - Flujo 1 - Chatbot 1
+        ArrayList<String> keywordOp1F1CB1 = new ArrayList<>(Arrays.asList("USA", "Estados Unidos", "New York"));
+        var op1F1CB1 = new Option_21065666_VeraRojas(1, "1) New York, Usa", 1, 2, keywordOp1F1CB1);
+
+        ArrayList<String> keywordOp2F1CB1 = new ArrayList<>(Arrays.asList("Paris", "Eiffel"));
+        var op2F1CB1 = new Option_21065666_VeraRojas(2, "2) París, Francia", 1, 1, keywordOp2F1CB1);
+
+        ArrayList<String>  keywordOp3F1CB1 = new ArrayList<>(Arrays.asList("Chile", "Torres", "Torres Paine", "Torres del Paine"));
+        var op3F1CB1 = new Option_21065666_VeraRojas(3, "3) Torres del Paine", 1, 1, keywordOp3F1CB1);
+
+        ArrayList<String> keywordOp4F1CB1 = new ArrayList<>(Arrays.asList("Regresar", "Salir", "Volver"));
+        var op4F1CB1 = new Option_21065666_VeraRojas(4, "4) Volver", 0, 1, keywordOp4F1CB1);
+
+        ArrayList<Option_21065666_VeraRojas> optionsFlow1CB1 = new ArrayList<>(Arrays.asList(op1F1CB1, op2F1CB1, op3F1CB1));
+        var f1CB1 = new Flow_21065666_VeraRojas(1, "Flujo 1 Chatbot 1\n¿Dónde te Gustaría ir?", optionsFlow1CB1);
+
+        f1CB1.flowAddOption(op4F1CB1);
+        System.out.println(f1CB1);
     }
 }
